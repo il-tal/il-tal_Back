3052643c018f7fd9365fbb0d98301117f68d193b
ggggraceful <109974940+ggggraceful@users.noreply.github.com>
2022-11-12 00:27:29 +0900

    Add files via upload

---
+++ src/main/java/com/example/sherlockescape/SherlockEscapeApplication.java
@@ -0,0 +1,13 @@
+package com.example.sherlockescape;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SherlockEscapeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SherlockEscapeApplication.class, args);
+	}
+
+}
