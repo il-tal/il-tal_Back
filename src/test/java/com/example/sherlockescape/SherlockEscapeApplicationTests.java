160d25d5c1e3672f77059f739b206de24cbf140b
ggggraceful <109974940+ggggraceful@users.noreply.github.com>
2022-11-12 00:27:29 +0900

    Add files via upload

---
+++ src/test/java/com/example/sherlockescape/SherlockEscapeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sherlockescape;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SherlockEscapeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
