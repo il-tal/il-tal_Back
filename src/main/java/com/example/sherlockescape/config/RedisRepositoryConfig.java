3938ce7b1db798a88eee319b5ff61b2cbacf5546
Liam-Genius <gijae8783@naver.com>
2022-11-14 00:53:31 +0900

    [Feat] add getAllCompany

--- src/main/java/com/example/sherlockescape/config/RedisRepositoryConfig.java
+++ src/main/java/com/example/sherlockescape/config/RedisRepositoryConfig.java
@@ -9,7 +9,6 @@ import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactor
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.repository.configuration.EnableRedisRepositories;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
-
 @RequiredArgsConstructor
 @Configuration
 @EnableRedisRepositories
--- src/main/java/com/example/sherlockescape/service/CompanyService.java
+++ src/main/java/com/example/sherlockescape/service/CompanyService.java
@@ -2,17 +2,26 @@ package com.example.sherlockescape.service;
 
 import com.amazonaws.services.s3.AmazonS3Client;
 import com.example.sherlockescape.domain.Company;
+import com.example.sherlockescape.domain.CompanyLike;
+import com.example.sherlockescape.domain.Theme;
 import com.example.sherlockescape.dto.ResponseDto;
 import com.example.sherlockescape.dto.request.CompanyRequestDto;
+import com.example.sherlockescape.dto.response.AllResponseDto;
+import com.example.sherlockescape.repository.CompanyLikeRepository;
 import com.example.sherlockescape.repository.CompanyRepository;
+import com.example.sherlockescape.repository.ThemeRepository;
 import com.example.sherlockescape.utils.CommonUtils;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
@@ -21,6 +30,10 @@ public class CompanyService {
 
     private final CompanyRepository companyRepository;
     private final AmazonS3Client amazonS3Client;
+
+    private final ThemeRepository themeRepository;
+
+    private final CompanyLikeRepository companyLikeRepository;
 
     @Value("${cloud.aws.s3.bucket}")
     private String bucketName;
@@ -37,10 +50,79 @@ public class CompanyService {
                 .companyName(companyRequestDto.getCompanyName())
                 .companyScore(companyRequestDto.getCompanyScore())
                 .location(companyRequestDto.getLocation())
-                .imgUrl(imgurl)
+                .companyImgUrl(imgurl)
                 .companyUrl(companyRequestDto.getCompanyUrl())
+                .address(companyRequestDto.getAddress())
+                .phoneNumber(companyRequestDto.getPhoneNumber())
+                .workHour(companyRequestDto.getWorkHour())
                 .build();
         companyRepository.save(company);
         return ResponseDto.success("업체 등록 성공");
     }
+    //    /*업체 정보 조회*/
+//    public List<CompanyResponseDto> getAll(){
+//
+//
+//        List<Company> companyList = companyRepository.findAll();
+//        List<CompanyResponseDto> allResponseDtoList = new ArrayList<>();
+//        for(Company company: companyList){
+//            Long companyId = company.getId();
+//
+//            Long companyLikeCnt = companyLikeRepository.countByCompanyId(companyId);
+//            Optional<CompanyLike> likes = companyLikeRepository.findByCompanyId(companyId);
+//            boolean likeCheck;
+//            likeCheck = likes.isPresent();
+//
+//            //댓글 수 추가
+//
+//            CompanyResponseDto allResponseDto =
+//                    CompanyResponseDto.builder()
+//                            .id(companyId)
+//                            .companyImgUrl(company.getImgUrl())
+//                            .location(company.getLocation())
+//                            .companyScore(company.getCompanyScore())
+//                            .companyUrl(company.getCompanyUrl())
+//                            .workHour(company.getWorkHour())
+//                            .phoneNumber(company.getPhoneNumber())
+//                            .address(company.getAddress())
+//                            .companyLikeCnt(companyLikeCnt)
+//                            .companyLikeCheck(likeCheck)
+//                            .build();
+//            allResponseDtoList.add(allResponseDto);
+//        }
+//        return allResponseDtoList;
+//    }
+    /*업체 정보 조회*/
+    public List<AllResponseDto> getAllCompany(Pageable pageable, String location){
+
+
+        List<Company> companyList = companyRepository.getCompanyList(pageable, location);
+        List<AllResponseDto> allResponseDtoList = new ArrayList<>();
+        for(Company company: companyList){
+            Long companyId = company.getId();
+
+            List<Theme> theme = themeRepository.findAllByCompanyId(companyId);
+            Long companyLikeCnt = companyLikeRepository.countByCompanyId(companyId);
+            Optional<CompanyLike> likes = companyLikeRepository.findByCompanyId(companyId);
+            boolean likeCheck;
+            likeCheck = likes.isPresent();
+            //댓글 수 추가
+            AllResponseDto allResponseDto =
+                    AllResponseDto.builder()
+                            .id(companyId)
+                            .companyImgUrl(company.getCompanyImgUrl())
+                            .location(company.getLocation())
+                            .companyScore(company.getCompanyScore())
+                            .companyUrl(company.getCompanyUrl())
+                            .workHour(company.getWorkHour())
+                            .phoneNumber(company.getPhoneNumber())
+                            .address(company.getAddress())
+                            .companyLikeCnt(companyLikeCnt)
+                            .companyLikeCheck(likeCheck)
+                            .themeList(theme).build();
+            allResponseDtoList.add(allResponseDto);
+        }
+        return allResponseDtoList;
+    }
+
 }
