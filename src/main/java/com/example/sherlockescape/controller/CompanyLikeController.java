3e71a3c50eff17d7e1be58d9aa1e13416e4eba52
Liam-Genius <gijae8783@naver.com>
2022-11-14 01:42:18 +0900

    [Feat] add companyLike

--- src/main/java/com/example/sherlockescape/controller/CompanyLikeController.java
+++ src/main/java/com/example/sherlockescape/controller/CompanyLikeController.java
@@ -1,4 +1,29 @@
 package com.example.sherlockescape.controller;
 
+import com.example.sherlockescape.dto.ResponseDto;
+import com.example.sherlockescape.dto.request.CompanyLikeRequestDto;
+import com.example.sherlockescape.dto.response.CompanyLikeResponseDto;
+import com.example.sherlockescape.security.user.UserDetailsImpl;
+import com.example.sherlockescape.service.CompanyLikeService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.validation.Valid;
+
+@RestController
+@RequestMapping("/company/wish")
+@RequiredArgsConstructor
 public class CompanyLikeController {
+
+    private final CompanyLikeService companyLikeService;
+
+    @PostMapping
+    public ResponseDto<CompanyLikeResponseDto> companyLikeUp(@RequestBody @Valid CompanyLikeRequestDto companyLikeRequestDto,
+                                                             @AuthenticationPrincipal UserDetailsImpl userDetailsImpl){
+        return companyLikeService.companyLikeUp(companyLikeRequestDto, userDetailsImpl.getMember().getId());
+    }
 }
