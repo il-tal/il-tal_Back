41dae19a67d813731954b6fd299559115bceae75
ggggraceful <109974940+ggggraceful@users.noreply.github.com>
2022-11-14 22:19:36 +0900

    [Chore] 일부 수정

--- src/main/java/com/example/sherlockescape/repository/ReviewRepository.java
+++ src/main/java/com/example/sherlockescape/repository/ReviewRepository.java
@@ -3,9 +3,11 @@ package com.example.sherlockescape.repository;
 
 import com.example.sherlockescape.domain.Review;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
+@Repository
 public interface ReviewRepository extends JpaRepository<Review, Long> {
 	List<Review> findAllByThemeId(Long themeId);
 }
