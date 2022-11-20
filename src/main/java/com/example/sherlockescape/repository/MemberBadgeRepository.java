6169a235ae56cda195d2b6ce2b31cea93f1f106b
Liam-Genius <gijae8783@naver.com>
2022-11-20 22:29:41 +0900

    [Feat] add get main achieve

--- src/main/java/com/example/sherlockescape/repository/MemberBadgeRepository.java
+++ src/main/java/com/example/sherlockescape/repository/MemberBadgeRepository.java
@@ -4,6 +4,10 @@ package com.example.sherlockescape.repository;
 import com.example.sherlockescape.domain.MemberBadge;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface MemberBadgeRepository extends JpaRepository<MemberBadge, Long> {
     MemberBadge findByBadgeId(Long badgeId);
+    List<MemberBadge> findAllByMemberId(Long memberId);
+
 }
--- src/main/java/com/example/sherlockescape/service/MemberBadgeService.java
+++ src/main/java/com/example/sherlockescape/service/MemberBadgeService.java
@@ -2,14 +2,24 @@ package com.example.sherlockescape.service;
 
 import com.example.sherlockescape.domain.Badge;
 import com.example.sherlockescape.domain.Member;
+import com.example.sherlockescape.domain.MemberBadge;
+import com.example.sherlockescape.domain.Review;
 import com.example.sherlockescape.dto.ResponseDto;
+import com.example.sherlockescape.dto.response.MainAchieveResponseDto;
 import com.example.sherlockescape.dto.response.UpdateBadgeResponseDto;
 import com.example.sherlockescape.repository.BadgeRepository;
+import com.example.sherlockescape.repository.MemberBadgeRepository;
 import com.example.sherlockescape.repository.MemberRepository;
+import com.example.sherlockescape.repository.ReviewRepository;
 import com.example.sherlockescape.utils.ValidateCheck;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
@@ -18,6 +28,8 @@ public class MemberBadgeService {
     private final ValidateCheck validateCheck;
     private final BadgeRepository badgeRepository;
     private final MemberRepository memberRepository;
+    private final ReviewRepository reviewRepository;
+    private final MemberBadgeRepository memberBadgeRepository;
 
 
     //대표 뱃지 수정
@@ -38,5 +50,37 @@ public class MemberBadgeService {
                                 .mainBadgeName(member.getMainBadgeName())
                                 .build();
         return ResponseDto.success(updateBadgeResponseDto);
+    }
+
+    //main achieve 전체 조회
+    public ResponseDto<MainAchieveResponseDto> getAchieve(Long memberId) {
+        Member member = validateCheck.getMember(memberId);
+
+        List<Review> reviewList = reviewRepository.findReviewsByMember(member);
+        List<MemberBadge> memberBadgeList = memberBadgeRepository.findAllByMemberId(memberId);
+        int totalAchieveCnt = 0;
+        int totalFailCnt = 0;
+        for(Review review: reviewList){
+            if(review.isSuccess()){
+                totalAchieveCnt += 1;
+            }else{
+                totalFailCnt +=1;
+            }
+        }
+        List<String> badgeImgUrl = new ArrayList<>();
+        for(MemberBadge memberBadge: memberBadgeList){
+            String badgeImg = memberBadge.getBadge().getBadgeImgUrl();
+            badgeImgUrl.add(badgeImg);
+        }
+        MainAchieveResponseDto mainAchieveResponseDto =
+                MainAchieveResponseDto.builder()
+                        .nickname(member.getNickname())
+                        .mainBadgeImg(member.getMainBadgeImg())
+                        .mainBadgeName(member.getMainBadgeName())
+                        .totalAchieveCnt(totalAchieveCnt)
+                        .totalFailCnt(totalFailCnt)
+                        .badgeImgUrl(badgeImgUrl)
+                        .build();
+        return ResponseDto.success(mainAchieveResponseDto);
     }
 }
