dd937c1dabf070cc88fd2265dbf13764ed0c138f
Liam-Genius <gijae8783@naver.com>
2022-11-23 15:55:05 +0900

    [Feat] add webEnvironment

--- src/test/java/com/example/sherlockescape/SherlockEscapeApplicationTests.java
+++ src/test/java/com/example/sherlockescape/SherlockEscapeApplicationTests.java
@@ -1,13 +1,13 @@
-package com.example.sherlockescape;
-
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-class SherlockEscapeApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
-
-}
+//package com.example.sherlockescape;
+//
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//
+//class SherlockEscapeApplicationTests {
+//
+//	@Test
+//	void contextLoads() {
+//	}
+//
+//}
