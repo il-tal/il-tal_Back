24dc44790b2b309feaf15c149deee31cae6d7189
Liam-Geni <66250121+Liam-Geni@users.noreply.github.com>
2022-11-24 02:06:02 +0900

    Update deploy.sh

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,26 +1,31 @@
-#!/bin/bash
-BUILD_JAR=$(ls /home/ubuntu/app/deploy/podor-*.jar)
-JAR_NAME=$(basename $BUILD_JAR)
-echo "> build 파일명: $JAR_NAME" >> /home/ubuntu/app/deploy/deploy.log
+#!/usr/bin/env bash
 
-echo "> build 파일 복사" >> /home/ubuntu/app/deploy/deploy.log
-DEPLOY_PATH=/home/ubuntu/app/deploy/
-cp $BUILD_JAR $DEPLOY_PATH
+REPOSITORY=/home/ubuntu/app
 
-IDLE_APPLICATION=$IDLE_PROFILE-$JAR_NAME
-IDLE_APPLICATION_PATH=$DEPLOY_PATH$IDLE_APPLICATION
+echo "> 현재 구동 중인 애플리케이션 pid 확인"
 
-ln -Tfs $DEPLOY_PATH$JAR_NAME $IDLE_APPLICATION_PATH
+CURRENT_PID=$(pgrep -fla java | grep hayan | awk '{print $1}')
 
-echo "> 현재 실행중인 애플리케이션 pid 확인" >> /home/ubuntu/app/deploy/deploy.log
+echo "현재 구동 중인 애플리케이션 pid: $CURRENT_PID"
 
-IDLE_PID=$(pgrep -f $IDLE_APPLICATION)
-
-if [ -z $IDLE_PID ]
-then
-  echo "> 현재 구동중인 애플리케이션이 없으므로 종료하지 않습니다." >> /home/ubuntu/app/deploy/deploy.log
+if [ -z "$CURRENT_PID" ]; then
+  echo "현재 구동 중인 애플리케이션이 없으므로 종료하지 않습니다."
+else
+  echo "> kill -15 $CURRENT_PID"
+  kill -15 $CURRENT_PID
+  sleep 5
 fi
 
-echo "> $IDLE_PROFILE 배포"    >> /home/ubuntu/app/deploy/deploy.log
-nohup java -jar -Duser.timezone=GMT+9 -Dspring.profiles.active=$IDLE_PROFILE $IDLE_APPLICATION_PATH >> /home/ubuntu/app/deploy/deploy.log 2>/home/ubuntu/app/deploy/deploy_err.log &  
-  
+echo "> 새 애플리케이션 배포"
+
+JAR_NAME=$(ls -tr $REPOSITORY/*SNAPSHOT.jar | tail -n 1)
+
+echo "> JAR NAME: $JAR_NAME"
+
+echo "> $JAR_NAME 에 실행권한 추가"
+
+chmod +x $JAR_NAME
+
+echo "> $JAR_NAME 실행"
+
+nohup java -jar -Duser.timezone=Asia/Seoul $JAR_NAME >> $REPOSITORY/nohup.out 2>&1 &
