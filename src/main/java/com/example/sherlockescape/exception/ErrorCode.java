b44a70851108bcc42e8adf521173d9f7915db6db
Liam-Genius <gijae8783@naver.com>
2022-11-25 15:08:04 +0900

    [Feat] update AOP

--- src/main/java/com/example/sherlockescape/exception/ErrorCode.java
+++ src/main/java/com/example/sherlockescape/exception/ErrorCode.java
@@ -11,6 +11,10 @@ public enum ErrorCode {
 
 	/* BAD_REQUEST 400 error*/
 	BAD_PASSWORD(HttpStatus.BAD_REQUEST.value(), "Password incorrect", "비밀번호를 확인하세요"),
+	TENDENCY_ALREADY_EXIST(HttpStatus.BAD_REQUEST.value(), "Tendency already exist", "성향을 이미 등록하셨습니다."),
+	MEMBER_BADGE_ALREADY_EXIST(HttpStatus.BAD_REQUEST.value(), "MemberBadge already exist", "이미 획득한 칭호 입니다."),
+	SUCCESS_NOT_ENOUGH(HttpStatus.BAD_REQUEST.value(), "Success count not enough", "성공 횟수가 부족합니다."),
+	FAIL_NOT_ENOUGH(HttpStatus.BAD_REQUEST.value(), "Fail count not enough", "실패 횟수가 부족합니다."),
 
 	/*UNAUTHORIZED 401 error*/
 
@@ -19,6 +23,12 @@ public enum ErrorCode {
 	AUTHOR_IS_DIFFERENT(HttpStatus.FORBIDDEN.value(), "You are not a writer", "회원님이 작성한 글이 아닙니다."),
 
 	/*Not Found 404 error*/
+	COMPANY_NOT_FOUND(HttpStatus.NOT_FOUND.value(), "Company not Found", "업체가 존재 하지 않습니다."),
+	THEME_NOT_FOUND(HttpStatus.NOT_FOUND.value(), "Theme not Found", "테마가 존재 하지 않습니다."),
+	BADGE_NOT_FOUND(HttpStatus.NOT_FOUND.value(), "Badge not Found", "뱃지가 존재 하지 않습니다."),
+	TENDENCY_NOT_FOUND(HttpStatus.NOT_FOUND.value(), "Tendency not Found", "성향이 존재 하지 않습니다."),
+	REVIEW_NOT_FOUND(HttpStatus.NOT_FOUND.value(), "Review not Found", "리뷰가 존재 하지 않습니다."),
+
 
 	/* CONFLICT 409 error*/
 	BAD_PASSWORD_CONFIRM(HttpStatus.CONFLICT.value(), "Password and PasswordConfirm don't match", "비밀번호와 비밀번호 확인이 다릅니다."),
