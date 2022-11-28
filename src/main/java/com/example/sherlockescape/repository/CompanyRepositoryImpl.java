ed78af7a3213a9308d4aab99a5ec3ab1cb7dddbc
soojin-dev <dvxz1125@gmail.com>
2022-11-28 18:33:51 +0900

    [Feat] 업체, 테마 필터링 후 결과 데이터 개수와 페이지 개수 반환 로직 추가

--- src/main/java/com/example/sherlockescape/repository/CompanyRepositoryImpl.java
+++ src/main/java/com/example/sherlockescape/repository/CompanyRepositoryImpl.java
@@ -4,6 +4,8 @@ import com.example.sherlockescape.domain.Company;
 import com.querydsl.core.types.dsl.BooleanExpression;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.Pageable;
 
 import java.util.List;
@@ -15,14 +17,21 @@ public class CompanyRepositoryImpl implements CompanyRepositoryCustom{
 
     private final JPAQueryFactory jpaQueryFactory;
     @Override
-    public List<Company> getCompanyList(Pageable pageable, String location) {
-        return jpaQueryFactory
+    public Page<Company> getCompanyList(Pageable pageable, String location) {
+        List<Company> result = jpaQueryFactory
                 .selectFrom(company)
                 .where(eqLocation(location))
                 .limit(pageable.getPageSize())
                 .offset(pageable.getOffset())
                 .orderBy(company.id.asc())
                 .fetch();
+
+        long totalSize = jpaQueryFactory
+                .selectFrom(company)
+                .where(eqLocation(location))
+                .fetch().size();
+
+        return new PageImpl<> (result, pageable, totalSize);
     }
     private BooleanExpression eqLocation(String location) {
         return location != null ? company.location.eq(location) : null;
