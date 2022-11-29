72216860588ff3c849d47f78ffb3206277e45eb5
Liam-Genius <gijae8783@naver.com>
2022-11-30 00:12:51 +0900

    [Feat] update oauth2

--- src/main/java/com/example/sherlockescape/controller/SocialLoginController.java
+++ src/main/java/com/example/sherlockescape/controller/SocialLoginController.java
@@ -17,6 +17,7 @@ public class SocialLoginController {
 
 	// 카카오 로그인
 	@GetMapping("/kakao/callback")
+	//-JIAlL23y9MDLlFj3QZ9d975eJujlOVm2iy_pzUItSE6xUvuVveeA-qwFio6P_wD3e4I9wo9dJcAAAGEwiHisg
 	public KakaoUserInfoDto kakaoLogin(@RequestParam(name="code") String code, HttpServletResponse response) throws JsonProcessingException {
 		return kakaoUserService.kakaoLogin(code, response);
 	}
